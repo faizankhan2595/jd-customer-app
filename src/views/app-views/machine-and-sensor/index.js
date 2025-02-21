d16797d0aead49132d4942b4527447d45242e686
HimanshuDTU18 <himu.delhi2015@gmail.com>
2025-02-21 14:07:05 +0530

    new

--- src/views/app-views/machine-and-sensor/index.js
+++ src/views/app-views/machine-and-sensor/index.js
@@ -12,6 +12,7 @@ import {
   Select,
   Card,
   Empty,
+  Checkbox,
 } from "antd";
 import {
   DeleteOutlined,
@@ -28,31 +29,45 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom";
 import { axiosInstance } from "App";
 import moment from "moment";
 import CardMachine from "./Card/CardMachine";
+import SubMenu from "antd/lib/menu/SubMenu";
 
 const MachineAndSensor = () => {
   const history = useHistory();
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axiosInstance.get(`api/web/machines?customer_id=${localStorage.getItem("parent_id")!="null"? localStorage.getItem("parent_id"):localStorage.getItem("user_id")}`);
-        if (response.status === 200) {
-          const responseData = response.data.items;
-          if (Array.isArray(responseData)) {
-            setData(responseData);
-          } else {
-            console.error("Unexpected response format:", responseData);
-          }
-          // console.log(JSON.stringify(responseData));
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+   
 
     fetchData();
   }, []);
+  const fetchData = async (health = "", overall = "") => {
+    let params = [];
+
+    if (health !== "" && health !== "all") {
+      params.push(`health=${health}`);
+    }
+
+    if (overall !== "" && overall !== "all") {
+      params.push(`machine_status=${overall}`);
+    }
+
+   
+    let url = params.length ? `&${params.join("&")}` : "";
+    try {
+      const response = await axiosInstance.get(`api/web/machines?customer_id=${localStorage.getItem("parent_id")!="null"? localStorage.getItem("parent_id"):localStorage.getItem("user_id")}${url}`);
+      if (response.status === 200) {
+        const responseData = response.data.items;
+        if (Array.isArray(responseData)) {
+          setData(responseData);
+        } else {
+          console.error("Unexpected response format:", responseData);
+        }
+        // console.log(JSON.stringify(responseData));
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   const deleteRow = async (id) => {
     console.log(id);
@@ -111,7 +126,111 @@ const MachineAndSensor = () => {
     </Menu>
   );
 
+  const [selectedStatus, setSelectedStatus] = useState("all");
+  const [overallStatus, setOverallStatus] = useState("all");
 
+  const FilterMenu = (
+    <Menu mode="horizontal">
+      {/* Status Filter */}
+      <SubMenu key="status" title="Health Status">
+        <Menu.Item key="status-all">
+          <Checkbox
+            checked={selectedStatus === 'all'}
+            onChange={() => {
+              setSelectedStatus('all');
+              fetchData("all", overallStatus);
+            }}
+          >
+            All
+          </Checkbox>
+        </Menu.Item>
+        {/* Satisfactory, Warning ya Critical */}
+        <Menu.Item key="status-good">
+          <Checkbox
+            checked={selectedStatus === 'Good'}
+            onChange={() => {
+              setSelectedStatus('Good');
+              fetchData('Good', overallStatus);
+            }}
+          >
+            Good
+          </Checkbox>
+        </Menu.Item>
+        <Menu.Item key="status-satisfactory">
+          <Checkbox
+            checked={selectedStatus === 'Satisfactory'}
+            onChange={() => {
+              setSelectedStatus('Satisfactory');
+              fetchData('Satisfactory', overallStatus);
+            }}
+          >
+            Satisfactory
+          </Checkbox>
+        </Menu.Item>
+        <Menu.Item key="status-warning">
+          <Checkbox
+            checked={selectedStatus === 'Warning'}
+            onChange={() => {
+              setSelectedStatus('Warning');
+              fetchData('Warning', overallStatus);
+            }}
+          >
+            Warning
+          </Checkbox>
+        </Menu.Item>
+        <Menu.Item key="status-critical">
+          <Checkbox
+            checked={selectedStatus === 'Critical'}
+            onChange={() => {
+              setSelectedStatus('Critical');
+              fetchData('Critical', overallStatus);
+            }}
+          >
+            Critical
+          </Checkbox>
+        </Menu.Item>
+      </SubMenu>
+
+      {/* Overall Status Filter */}
+      <SubMenu key="overall" title="Overall Status">
+        <Menu.Item key="overall-all">
+          <Checkbox
+            checked={overallStatus === 'all'}
+            onChange={() => {
+              setOverallStatus('all');
+              fetchData(selectedStatus, 'all');
+            }}
+          >
+            All
+          </Checkbox>
+        </Menu.Item>
+        <Menu.Item key="overall-active">
+          <Checkbox
+            checked={overallStatus === '1'}
+            onChange={() => {
+              setOverallStatus('1');
+              fetchData(selectedStatus, '1');
+            }}
+          >
+            Active
+          </Checkbox>
+        </Menu.Item>
+        <Menu.Item key="overall-inactive">
+          <Checkbox
+            checked={overallStatus === '0'}
+            onChange={() => {
+              setOverallStatus('0');
+              fetchData(selectedStatus, '0');
+            }}
+          >
+            Inactive
+          </Checkbox>
+        </Menu.Item>
+
+      </SubMenu>
+            
+    </Menu>
+  );
   return (
     <div>
       <h4>
